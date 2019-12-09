036abd96abf6e9b2e99973cd8de98ab3ac5c5ec5
David Correa <davidjonatan.c@gmail.com>
2019-12-09 01:05:49 -0300

    100%?

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -15,21 +15,22 @@ from torch.utils.data.sampler import SubsetRandomSampler
 # Params
 train_images_path = 'train/'
 train_labels_filename = 'train_labels.csv'
-data_size = 200 # 0 to use every picture.
+data_size = 500
 
-def preprocess(image):
-    resize = transforms.Resize((64, 64))
-    image = resize(image)
-    image = np.array(image)
-    # plt.imshow(image)
-    # plt.pause(0.001)
-    image = image.transpose(2,1,0)
-    return image
+# Transforms
+image_transforms = transforms.Compose([
+    transforms.Resize(256),
+    transforms.ColorJitter(),
+    transforms.RandomCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize(128),
+    transforms.ToTensor()
+])
 
+# Dataset
 class CatsAndDogsDataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, label_source, data_size = 0):
-        files = os.listdir(data_dir)
-        files = [os.path.join(data_dir,x) for x in files]
+    def __init__(self, data_dir, label_source, data_size = 0, transform = None):
+        files = [os.path.join(data_dir, x) for x in os.listdir(data_dir)]
         if data_size < 0 or data_size > len(files):
             assert("Data size should be between 0 to number of files in the dataset")
         if data_size == 0:
@@ -37,6 +38,7 @@ class CatsAndDogsDataset(torch.utils.data.Dataset):
         self.data_size = data_size
         self.files = random.sample(files, self.data_size)
         self.label_source = label_source
+        self.transform = transform
 
     def __len__(self):
         return self.data_size
@@ -44,56 +46,42 @@ class CatsAndDogsDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         image_address = self.files[idx]
         image = Image.open(image_address)
-        image = preprocess(image)
-        image = torch.Tensor(image)
-        image = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))(image)
+        image = self.transform(image)
+        return image, self.__imagelabel__(image_address)
+
+    def __imagelabel__(self, image_address):
         file_name = image_address[:-4].split("/")[1]
         label_idx = int(file_name[4:])
         label = self.label_source[label_idx]
         label = torch.tensor(label).long()
-        return image, label
+        return label
 
+# Labels
 labels = pd.read_csv(train_labels_filename)
-lenc = LabelEncoder()
-labels_numeros = labels['label'] = lenc.fit_transform(labels['label'])
+labels_encoder = LabelEncoder()
+labels_numeros = labels_encoder.fit_transform(labels['label'])
 
-train_set = CatsAndDogsDataset(data_dir = train_images_path, label_source = labels_numeros, data_size = data_size)
+cats_and_dogs_dataset = CatsAndDogsDataset(data_dir = train_images_path, label_source = labels_numeros, data_size = data_size, transform = image_transforms)
 
-batch_size = 16
-scale = 0.1
+batch_size = 84
 test_proportion = .2
+train_size = int((1-test_proportion) * len(cats_and_dogs_dataset))
+test_size = len(cats_and_dogs_dataset) - train_size
 
-trainset_size = len(train_set)
-indices = list(range(trainset_size))
-split = int(np.floor(test_proportion * trainset_size))
-train_indices, test_indices = indices[split:], indices[:split]
+# Datasets and Loaders
+train_dataset, test_dataset = torch.utils.data.random_split(cats_and_dogs_dataset, [train_size, test_size])
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
 
-train_sampler = SubsetRandomSampler(train_indices)
-test_sampler = SubsetRandomSampler(test_indices)
-
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=False, sampler = train_sampler, num_workers=0)
-test_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=False, sampler = test_sampler, num_workers=0)
-
+# Net
 class CatsAndDogsNet(nn.Module):
     def __init__(self):
         super(CatsAndDogsNet, self).__init__()
 
-        # Convultional Net
-
-        # Entrada: 64x64
-        # Salida: 60x60
         self.conv1 = nn.Conv2d(3, 64, 5)
-
-        # Entrada: 60x60
-        # Salida: 30x30
         self.pool = nn.MaxPool2d(2, 2)
-
-        # Entrada: 30x30
-        # Salida: 24x24
         self.conv2 = nn.Conv2d(64, 32, 5)
-
-        # Fully connected network
-        self.fc1 = nn.Linear(32 * 13 * 13, 120)
+        self.fc1 = nn.Linear(32 * 29 * 29, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 2)
 
@@ -101,17 +89,14 @@ class CatsAndDogsNet(nn.Module):
         x = self.pool(torch.relu(self.conv1(x)))
         x = self.pool(torch.relu(self.conv2(x)))
         # print(x.shape)
-        x = x.view(-1, 32 * 13 * 13)
+        x = x.view(-1, 32 * 29 * 29)
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
-        return x
-    
+        return x  
+
 model = CatsAndDogsNet()
 
-training = False
-
-#Función que modela el entrenamiento de la red
 def train(model, data_loader, optimizer):
     #El modelo se debe poner en modo training
     model.train()
@@ -176,13 +161,10 @@ validation_loss = []
 
 #Entrenamiento. Por default lo hacemos por 100 iteraciones (epochs) 
 epochs = 100
-
-for epoch in range(0, epochs):
-    print("Training...")
+for epoch in range(1, epochs + 1):
     #Hacemos el train con los datos que salen del loader
     train_loss = train(model, train_loader, optimizer)
     
-    print("Testing...")
     #Probamos el nuevo entrenamiento sobre los datos de test
     test_loss, accuracy = test(model, test_loader)
     
@@ -191,8 +173,39 @@ for epoch in range(0, epochs):
     training_loss.append(train_loss)
     validation_loss.append(test_loss)
     
-    #Imprimimos los resultados parciales
-    print('Epoch {:d}: loss entrenamiento= {:.4f}, loss validacion= {:.4f}, exactitud={:.4%}'.format(epoch, train_loss, test_loss, accuracy))
+    #Cada 10 iteraciones vamos imprimiendo nuestros resultados parciales
+    if (epoch) % 10 == 0:
+        print('Epoch {:d}: loss entrenamiento= {:.4f}, loss validacion= {:.4f}, exactitud={:.4%}'.format(epoch, train_loss, test_loss, accuracy))
 
-#for param_tensor in model.state_dict():
-#    print(param_tensor, "\n", model.state_dict()[param_tensor].numpy())
+#Creamos la matriz de confusión, esta es parte del paquete scikit
+from sklearn.metrics import confusion_matrix
+
+#Ponemos el modelo en modo evaluación
+model.eval()
+
+#Hacemos las predicciones para los datos de test
+#Para eso, en primer lugar generamos la matriz de entradas y vector de 
+#resultados a partir del dataloader
+entradas = list()
+salidas = list()
+for batch,tensor in enumerate(test_loader):   
+    valor,salida = tensor
+    entradas.append(valor)
+    salidas.append(salida)
+#Se pasan a formato Tensor
+entradas = torch.cat(entradas)
+salidas = torch.cat(salidas)
+#Se obtienen las predicciones
+_, predicted = torch.max(model(entradas), 1)
+
+#Graficamos la matriz de confusión
+cm = confusion_matrix(salidas.numpy(), predicted.numpy())
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.colorbar()
+tick_marks = np.arange(10)
+
+plt.xticks(tick_marks, labels_encoder.inverse_transform(range(2)), rotation=45)
+plt.yticks(tick_marks, labels_encoder.inverse_transform(range(2)))
+plt.xlabel("El modelo predijo que era")
+plt.ylabel("La imágen real era")
+plt.show()
